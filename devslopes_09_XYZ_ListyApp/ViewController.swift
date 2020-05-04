71f3b1cdd472d74ac8fa1d7b2ee425eaaf64e9d3
mdmunifhasan8520 <mdmunifhasan8520@gmail.com>
2020-05-04 13:47:55 +0600

    create First portion of this app.

--- devslopes_09_XYZ_ListyApp/ViewController.swift
+++ devslopes_09_XYZ_ListyApp/ViewController.swift
@@ -7,14 +7,44 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var xLbl: UILabel!
+    @IBOutlet weak var yLbl: UILabel!
+    @IBOutlet weak var zLbl: UILabel!
+    
+    var motionManager: CMMotionManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        motionManager = CMMotionManager()
+        
+        motionManager.startAccelerometerUpdates(to: .main, withHandler: updateLabels)
     }
-
+    
+    func updateLabels(data: CMAccelerometerData?, error:Error?) {
+        guard let acclerometerData = data else { return }
+        //print(acclerometerData)
+        
+        let numberFormatter = NumberFormatter()
+        numberFormatter.minimumFractionDigits = 1
+        numberFormatter.maximumFractionDigits = 1
+        
+        let x = numberFormatter.string(for: acclerometerData.acceleration.x)!
+        let y = numberFormatter.string(for: acclerometerData.acceleration.y)!
+        let z = numberFormatter.string(for: acclerometerData.acceleration.z)!
+        print(x,y,z)
+        
+        xLbl.text = "X: \(x)"
+        yLbl.text = "Y \(y)"
+        zLbl.text = "Z \(z)"
+        
+        
+    }
+    
+    
+    
 
 }
 
