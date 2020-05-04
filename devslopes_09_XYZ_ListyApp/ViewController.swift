ae657f15a940b87574f937adfe14ad8161982675
mdmunifhasan8520 <mdmunifhasan8520@gmail.com>
2020-05-04 12:11:21 +0600

    Initial Commit

---
+++ devslopes_09_XYZ_ListyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  devslopes_09_XYZ_ListyApp
+//
+//  Created by Mac PC on 5/4/20.
+//  Copyright © 2020 Ticon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
